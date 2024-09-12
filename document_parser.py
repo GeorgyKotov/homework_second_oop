e35288ee2a52097c272be2ae65c1efcf543a0a40
Georgy Kotov <g.kotov@corp.nekki.ru>
2024-09-12 13:01:41 +0300

    refactor documents parser

--- document_parser.py
+++ document_parser.py
@@ -1,24 +1,34 @@
+import os
 from operator import itemgetter
 
 
-line_count_first = sum(1 for line in open('1.txt', encoding='utf-8'))
-line_count_second = sum(1 for line in open('2.txt', encoding='utf-8'))
-line_count_third = sum(1 for line in open('3.txt', encoding='utf-8'))
-documents = {'1.txt': line_count_first, '2.txt': line_count_second, '3.txt': line_count_third}
-sorted_documents = dict(sorted(documents.items(), key=itemgetter(1)))
+def get_dict_documents(name_directory: str):
+    file_path = os.path.join(os.getcwd(), name_directory)
+    documents_list = os.listdir(file_path)
+    documents_dict = {}
+    for document in documents_list:
+        line_count = sum(1 for line in open(file_path + f'\\{document}', encoding='utf-8'))
+        documents_dict[document] = line_count
+    return dict(sorted(documents_dict.items(), key=itemgetter(1)))
 
-for document in sorted_documents:
-    with open(document, encoding='utf-8') as file_initial:
-        lines_list = file_initial.readlines()
-        lines_count_in_document = len(lines_list)
-    with open('final_text.txt', 'a', encoding='utf-8') as file_final:
-        file_final.write(document + '\n')
-        file_final.write(f'{lines_count_in_document}' + '\n')
-    for line in lines_list:
-        with open('final_text.txt', 'a', encoding='utf-8') as file_final:
-            file_final.write(line)
-    with open('final_text.txt', 'a', encoding='utf-8') as file_final:
-        file_final.write("\n")
 
+def final_file_completion(name_directory_files: str, name_final_file: str):
+    documents_list = get_dict_documents(name_directory_files)
+    for document in documents_list:
+        file_path = os.path.join(os.getcwd() + f'\\{name_directory_files}' + f'\\{document}')
+        with open(file_path, encoding='utf-8') as file_initial:
+            lines_list = file_initial.readlines()
+            lines_count_in_document = len(lines_list)
+        with open(name_final_file, 'a', encoding='utf-8') as file_final:
+            file_final.write(document + '\n')
+            file_final.write(f'{lines_count_in_document}' + '\n')
+        for line in lines_list:
+            with open(name_final_file, 'a', encoding='utf-8') as file_final:
+                file_final.write(line)
+        with open(name_final_file, 'a', encoding='utf-8') as file_final:
+            file_final.write("\n")
+
+
+final_file_completion('documents', 'final_text.txt')
 
 
